908fd0d78847c208885dc6c725d9f4bf36edfe4f
mariasilva <marcia-nasare@hotmail.com>
2015-11-19 17:31:43 -0200

    sec commit

--- tests/angularTest.js
+++ tests/angularTest.js
@@ -1,41 +1,11 @@
 
-describe('PhoneListCtrl', function(){
-
-  beforeEach(module('firstApp'));
-
-  it('should create "phones" model with 3 phones', inject(function($controller) {
-    var scope = {},
-        ctrl = $controller('PhoneListCtrl', {$scope:scope});
-
-    expect(scope.phones.length).toBe(3);
-  }));
-
-});
-
-/*describe('PhoneListCtrl', function(){
-
-	beforeEach(module('firstApp'));
-
-	jasmine.addMatchers({
-		moreThanTenPoints : function(arr){
-			var ok = false;
-			for (var i = 0; i < arr.length; i++) {
-				ok = arr[i] > 10 ? true : false;
-			}
-			return ok;
-		};
-
-	});
-
-	it('Should have team and points', inject(function($controller){
+describe('indexCtrl', function(){
+	beforeEach(module('root'));
+	it('Should update values after user choice', inject(function($controller){
 		var scope = {};
-		ctrl = $controller('PhoneListCtrl', {$scope:scope});
-
-		var checkList = scope.driversList;
-		var arrPoints = [];
-		for (var Driver in checkList){
-			arrPoints.add(Driver.points);
-		}
-		expect(moreThanTenPoints).toBe(true);
+		ctrl = $controller('indexCtrl', {$scope:scope});
+		expect(scope.favoriteShape == 'shape').toBeFalsy();
+		expect(scope.favoriteColor == 'color').toBeFalsy();
+		expect(scope.favoriteWord == 'word').toBeFalsy();
 	}));
-});*/
+});
